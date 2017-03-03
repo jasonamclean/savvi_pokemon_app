75f1c50b2c1894225d3e0a3197e6c2edf8920cef
Jason McLean <jasonamclean@hotmail.com>
2017-03-03 16:58:37 -0700

    Fix empty search field error and update search field and header font

--- Gemfile
+++ Gemfile
@@ -53,3 +53,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'httparty'
+gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,11 @@ class HomeController < ApplicationController
     if request.GET.include? 'pokesearch'
       url = "http://pokeapi.co/api/v2/pokemon/" + params[:pokesearch]
       @data = ActiveSupport::JSON.decode( HTTParty.get(url, headers: {'content_type' => 'application/json'} ).to_json )
-      
+
       url = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=" + @data['name']
       @giphy = ActiveSupport::JSON.decode( HTTParty.get(url, headers: {'content_type' => 'application/json'} ).to_json )
+    else
+      
     end
   end
 end
